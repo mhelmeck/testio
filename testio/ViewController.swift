890190a59ced051e91ec73d3f938d5f2535cb184
Maciek <mahe03@betssongroup.com>
2024-08-22 10:16:50 +0200

    Remove main storyboard

--- testio/ViewController.swift
+++ testio/ViewController.swift
@@ -1,19 +1,10 @@
-//
-//  ViewController.swift
-//  testio
-//
-//  Created by Maciej Helmecki on 22/08/2024.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .green
     }
-
-
 }
-
